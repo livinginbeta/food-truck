7bef3cbc9685e9cdb866de7daf029fc6fc3bd69e
unknown <dyirchott@hotmail.com>
2020-09-19 17:21:35 -0400

    Updated ItemController

--- src/main/java/com/casestudy/foodtruck/ItemConfig.java
+++ src/main/java/com/casestudy/foodtruck/ItemConfig.java
@@ -25,6 +25,6 @@ public class ItemConfig {
     public void setup() {
                 itemBuilder().create(new Item(1L,"California Burrito", "Carne asada, cheese, salsa fresca, guacamole, and french fries", 5.95));
                 itemBuilder().create(new Item(2L, "Oyakodon", "literally \"parent-and-child donburi\", chicken (the parent), egg (the child), sliced scallion, and seasoned with soy sauce, mirin, and dashi", 6.95));
-                itemBuilder().create(new Item (3L, "Unadon", "Fillets of grilled eel glazed with a sweetened soy-based sauce and caramelized over charcoal fire then served over steamed white rice", 8.95));
+                itemBuilder().create(new Item(3L, "Unadon", "Fillets of grilled eel glazed with a sweetened soy-based sauce and caramelized over charcoal fire then served over steamed white rice", 8.95));
     }
 }
--- src/main/java/com/casestudy/foodtruck/controller/ItemController.java
+++ src/main/java/com/casestudy/foodtruck/controller/ItemController.java
@@ -1,10 +1,15 @@
 package com.casestudy.foodtruck.controller;
 
 
+import com.casestudy.foodtruck.model.Item;
 import com.casestudy.foodtruck.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/menu")
@@ -16,8 +21,29 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<Item> create(@RequestBody Item itemToBeCreated) {
+        return new ResponseEntity<>(itemService.create(itemToBeCreated), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/read/all")
+    public ResponseEntity<List<Item>> readAll() {
+        return new ResponseEntity<>(itemService.readAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/read/{id}")
+    public ResponseEntity<Item> readById(@PathVariable Long id) {
+        return new ResponseEntity<>(itemService.readById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Item> update(@PathVariable Long id, @RequestBody Item newData) {
+        return new ResponseEntity<>(itemService.updateById(id, newData), HttpStatus.OK);
+        }
 
 
-
-
+    @DeleteMapping("/delete/{id}")
+        public ResponseEntity<Item> delete(@PathVariable Long id) {
+        return new ResponseEntity<>(itemService.deleteById(id), HttpStatus.OK);
+        }
 }
