37711371b08008c1e10bd63e6c586d30945ff881
unknown <dyirchott@hotmail.com>
2020-09-26 16:18:14 -0400

    purchased elements for logo and updated items list

--- src/main/java/com/casestudy/foodtruck/ItemConfig.java
+++ src/main/java/com/casestudy/foodtruck/ItemConfig.java
@@ -18,11 +18,11 @@ public class ItemConfig {
         itemService.create(new Item(1L, "Chocolate Frosted", "Classic cake donut topped with homemade chocolate icing", 1.00));
         itemService.create(new Item(2L, "Glazed and Confused", "A classic glazed donut so delicious it'll make you wonder why you didn't buy more", 1.00));
         itemService.create(new Item(3L, "Boston Cream", "This round, Bavarian cream-filled donut has chocolate frosting and will make you want to pahk your cah so you can stay and eat more!", 1.00));
-        itemService.create(new Item(4L, "Crueller", "Classic cake donut topped with homemade chocolate icing", 1.00));
-        itemService.create(new Item(5L, "Bismark", "Classic cake donut topped with homemade chocolate icing", 1.00));
-        itemService.create(new Item(6L, "Fritter", "Classic cake donut topped with homemade chocolate icing", 1.00));
-        itemService.create(new Item(7L, "Powdered Donut", "Classic cake donut topped with homemade chocolate icing", 1.00));
-        itemService.create(new Item(8L, "Old-Fashioned", "Classic cake donut topped with homemade chocolate icing", 1.00));
+        itemService.create(new Item(4L, "Cruller De Ville", "There are 101 reasons why this ring of fried sweet dough is the right choice.", 1.00));
+        itemService.create(new Item(5L, "Ich bin ein Berliner", "There is no 'ick' in this jelly-filled pastry. We are convinced that in his historic speech JFK meant he was one of these.", 1.00));
+        itemService.create(new Item(6L, "Frittered Away", "Don't squander your chance to have one or more of these battered sweet dough treats..", 1.00));
+        itemService.create(new Item(7L, "With Great Powder Comes Great Responsibiliy", "This super-powdered cake donut will make you feel like a hero.", 1.00));
+        itemService.create(new Item(8L, "Old-Fashioned", "This classic is still around for a reason. Enjoy this sour cream cake donut while feeling sophisticated and mature. Monocle not included.", 1.00));
         itemService.setupAll();
         
     }
